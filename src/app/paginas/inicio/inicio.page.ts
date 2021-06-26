7e8a7e72b15f9b646fdf669fc336a54dada14c79
KatherineSalas <katherine.salas.baeza@cftmail.cl>
2021-06-26 14:41:38 -0400

    listas

--- src/app/paginas/inicio/inicio.page.ts
+++ src/app/paginas/inicio/inicio.page.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente{
+  icono: string;
+  nombre: string;
+  direccion: string;
+
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
+
+componente: Componente[] = [
+  {
+    icono: 'ellipsis-vertical-outline',
+    nombre: "Registrar Usuarios",
+    direccion:'/r-usuarios'
+
+  }
+
+];
 
   constructor() { }
 
